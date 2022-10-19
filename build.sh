0994bc43b07e74fbcd00d263caab6eb7f6ca1a49
Noam Prag <noam.prag0@gmail.com>
2022-10-19 15:56:42 +0300

    Removed all unnecessary files

--- build.sh
+++
@@ -1,5 +0,0 @@
-source venv/bin/activate
-manim_revealjs_version=0.0.3
-# to install these scripts, run `pip install --upgrade build twine``
-python -m build
-python -m twine upload dist/*$manim_revealjs_version*
